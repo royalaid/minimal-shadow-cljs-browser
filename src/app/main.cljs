756db4aa4a0df91ebb41c7eab4064c4886efc318
Mark Aiken <gwmaiken@att.net>
2019-10-08 13:28:08 -0700

    Add missing pieces to enable reagent

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,12 +1,21 @@
 
 (ns app.main
-  (:require [app.lib :as lib]))
+  (:require [app.lib :as lib]
+            [reagent.core :as r]))
 
 (def a 1)
 
 (defonce b 2)
 
+(defonce greeting "hello")
+(def who "world")
+
+(defn app []
+  [:div greeting " " who])
+
 (defn main! []
+  (r/render [app]
+            (.getElementById js/document "app"))
   (println "[main]: loading"))
 
 (defn reload! []
